065a79a8d884a1f48938e80d068deca5b8f4bdcf
huhaolei <hhl913169083@outlook.com>
2023-05-16 14:37:45 +0800

    线性表2

--- listforpython.py
+++ listforpython.py
@@ -57,37 +57,87 @@ def main():
         print('四，查找给定值的元素')
         print('****************************************')
         # print('请输入你的操作（1，2，3，4）')
-        i=input('请输入你的操作（1，2，3，4）:')
+        i=input('请输入你的操作数（1，2，3，4）:')
+        #在python3中input输入是string类型的，一定要注意 不是int类型的
+        # i=int(i)
         #python中没有switch函数 使用match函数
-        #match函数的用法
-        match i:
-            case 1:
-                longth=input('请输入新创建的线性表的长度')
-                L=makeempty(longth)
-            case 2:
-                #  insert(x,i,L):插入一个新元素
-                x=input("请输入要插入的元素：")
-                i=input('请输入要插入的元素位置')
-                if insert(x,i,L):
-                    print('插入成功！')
-                else:
-                    print('插入失败！')
-            case 3:
-                #delete(i,L):删除某个位置上的元素
-                i=input('请输入要删除元素的位置')
-                if delete(i,L):
-                    print('删除成功！')
-                else:
-                    print('删除失败！')
+        
+        if i=='1':
+            longth=input('请输入新创建的线性表的长度')
+            L=makeempty(longth)
+            # return True
+        elif i=='2':
+            #  insert(x,i,L):插入一个新元素
+            x=input("请输入要插入的元素：")
+            i=input('请输入要插入的元素位置')
+            if insert(x,i,L):
+                print('插入成功！')
+                # return True
+            else:
+                print('插入失败！')
+                return False
+        elif i=='3':
+            i=input('请输入要删除元素的位置')
+            if delete(i,L):
+                print('删除成功！')
+                # return True
+            else:
+                print('删除失败！')
+                # return False
 
-            case 4:
-                #find(x,L):给出定值查找元素
-                x=input('请输入要查找的元素')
-                if find(x,L):
-                    print(find(x,L))
-                    print('查找成功！')
-                else:
-                    print('查找失败！')
+        elif i=='4':               
+             #find(x,L):给出定值查找元素
+            x=input('请输入要查找的元素')
+            if find(x,L):
+                print(find(x,L))
+                print('查找成功！')
+                # return True
+            else:
+                print('查找失败！')
+                # return False
+        else:
+            print('输入不合法，请重新输入')
+
+        #match函数的用法
+        # match i:
+        #     case 1:
+        #         longth=input('请输入新创建的线性表的长度')
+        #         L=makeempty(longth)
+        #         return True
+        #     case 2:
+        #         #  insert(x,i,L):插入一个新元素
+        #         x=input("请输入要插入的元素：")
+        #         i=input('请输入要插入的元素位置')
+        #         if insert(x,i,L):
+        #             print('插入成功！')
+        #             return True
+        #         else:
+        #             print('插入失败！')
+        #             return False
+        #     case 3:
+        #         #delete(i,L):删除某个位置上的元素
+        #         i=input('请输入要删除元素的位置')
+        #         if delete(i,L):
+        #             print('删除成功！')
+        #             return True
+        #         else:
+        #             print('删除失败！')
+        #             return False
+
+        #     case 4:
+        #         #find(x,L):给出定值查找元素
+        #         x=input('请输入要查找的元素')
+        #         if find(x,L):
+        #             print(find(x,L))
+        #             print('查找成功！')
+        #             return True
+        #         else:
+        #             print('查找失败！')
+        #             return False
+        #     case _:
+        #         print('输入不合法，请重新输入')
+        #         return False
+            
 
 if __name__=='__main__':
     sys.exit(main())
