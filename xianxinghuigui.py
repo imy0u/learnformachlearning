aabfad84761f6d9ee56520991ec98df0e97e2950
huhaolei <hhl913169083@outlook.com>
2023-06-02 19:50:12 +0800

    线性回归到小批量梯度下降

--- xianxinghuigui.py
+++ xianxinghuigui.py
@@ -15,16 +15,26 @@ import random
 '''
 构造人工数据集
 '''
-num_inputs=2
-num_examples=1000
-true_w=[2,-3.4]
+# num_inputs=2
+# num_examples=1000
+# true_w=[2,-3.4]
+# true_b=4.2
+# features=torch.from_numpy(np.random.normal(0,1,(num_examples,num_inputs)))
+# #features是每一行是一个长度为2的向量
+# labels=true_w[0]*features[:,0]+true_w[1]*features[:,1]+true_b
+# #labels是每一行是一个长度为1的向量（标量）
+# labels+=torch.from_numpy(np.random.normal(0,0.01,size=labels.size()))
+# # print(features[0],labels[0])
+
+def synthetic_data(w,b,num_examples):
+    '''y=Xw+b+噪音.'''
+    X=torch.normal(0,1,(num_examples,len(w)))#均值为0，方差为1的随机数
+    y=torch.matmul(X,w)+b#Xw+b
+    y+=torch.normal(0,0.01,y.shape)#均值为0，方差为0.01，形状和y一样
+    return X,y.reshape((-1,1))#reshape(-1,1)转化为1行
+true_w=torch.tensor([2,-3.4])
 true_b=4.2
-features=torch.from_numpy(np.random.normal(0,1,(num_examples,num_inputs)))
-#features是每一行是一个长度为2的向量
-labels=true_w[0]*features[:,0]+true_w[1]*features[:,1]+true_b
-#labels是每一行是一个长度为1的向量（标量）
-labels+=torch.from_numpy(np.random.normal(0,0.01,size=labels.size()))
-# print(features[0],labels[0])
+features,labels=synthetic_data(true_w,true_b,10000000)
 
 def use_svg_display():
     # display.set_matplotlib_formats('svg')
@@ -40,16 +50,18 @@ def showshowkan():#展示
     plt.scatter(features[:,1].numpy(),labels.numpy(),1)
     plt.show()
 
-def data_iter(batch_size,features,labels):
+def data_iter(batch_size,features,labels):#每次输出batch_size个
     num_examples=len(features)
-    indices=list(range(num_examples))
-    random.shuffle(indices)
-    for i in range(0,num_examples,batch_size):
-        j=torch.LongTensor(indices[i:min(i+batch_size,num_examples)])
-        yield features.index_select(0,j),labels.index_select(0,j)
-
+    indices=list(range(num_examples))#生成样本长度个数列
+    random.shuffle(indices)#将样本顺序随机打乱
+    for i in range(0,num_examples,batch_size):#每次跳batch_size个大小
+        batch_indices=torch.tensor(indices[i:min(i+batch_size,num_examples)])
+        yield features[batch_indices],labels[batch_indices]
+#yield是return的同胞兄弟
+# yield返回yield的函数则返回一个可迭代的 generator（生成器
+# ）对象，你可以使用for循环或者调用next(
+# )方法遍历生成器对象来提取结果。
 def read_shuju():
-    
     for X,y in data_iter(batch_size,features,labels):
         print(X,y)
         break
@@ -63,14 +75,22 @@ def linreg(X, w, b):  #@save
     return torch.matmul(X, w) + b
 '''定义损失函数'''
 def squared_loss(y_hat,y):
+    '''
+    均方误差
+    没有算均值
+    '''
     return(y_hat-y.reshape(y_hat.shape))**2/2
 '''定义优化算法'''
 def sgd(params, lr, batch_size):  #@save
     """小批量随机梯度下降"""
-    with torch.no_grad():
+    with torch.no_grad():#这里表示不自动求导，自动设置为false
         for param in params:
-            param -= lr * param.grad / batch_size
+            param -= lr * param.grad / batch_size#步伐*
             param.grad.zero_()
+            '''
+            斜率和梯度的区别
+            梯度是一个方向向量，表示函数在该点沿着这个方向变化最快
+            '''
 '''训练'''
 lr = 0.03
 num_epochs = 3
