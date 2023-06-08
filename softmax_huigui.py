6c62f9a1c3145c896ce7924216fa9299c1f95e53
huhaolei <hhl913169083@outlook.com>
2023-06-08 18:21:06 +0800

    softmax

--- softmax_huigui.py
+++ softmax_huigui.py
@@ -8,6 +8,7 @@ from torch.utils import data
 from torchvision import transforms
 from d2l import torch as d2l
 import matplotlib.pyplot as plt
+from IPython import display
 
 '''下载数据集'''
 d2l.use_svg_display()
@@ -37,15 +38,23 @@ def show_images(imgs,num_rows,num_cols,titles=None,scale=1.5):
         ax.axes.get_yaxis().set_visible(False)
         if titles:
             ax.set_title(titles[i])
-    plt.show()
+    # plt.show()
     return axes
 X,y=next(iter(data.DataLoader(mnist_train,batch_size=18)))
 show_images(X.reshape(18,28,28),2,9,titles=get_fashion_mnist_labels(y))
-plt.show()
+
 '''读取小批量数据，大小为batch_size'''
 batch_size=256
 
 def get_dataloader_workers():
     '''使用四个进程来读取数据
     '''
-    return
+    return 4
+
+train_iter=data.DataLoader(mnist_train,batch_size,shuffle=True,num_workers=get_dataloader_workers())
+
+timer=d2l.Timer()
+for X,y in train_iter:
+    continue
+print(f'{timer.stop():2f} sec')
+
