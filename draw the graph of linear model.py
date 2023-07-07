8144aaf4b35ff76496bbcf05d9c26cbb2729114b
huhaolei <hhl913169083@outlook.com>
2023-07-07 16:05:28 +0800

    draw the graph of linear model

---
+++ draw the graph of linear model.py
@@ -0,0 +1,35 @@
+'''draw the graph of 线性模型 linear model
+假设模型，
+控制w的大小，使得mse不断变化
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_data=[1.0,2.0,3.0]
+y_data=[2.0,4.0,6.0]
+
+def forward(x):
+    return x*w
+
+def loss(x,y):
+    y_pred=forward(x)
+    return (y_pred-y)*(y_pred-y)
+
+w_list=[]
+mse_list=[]
+for w in np.arange(0.0,4.1,0.1):
+    print('w=',w)
+    l_sum=0
+    for x_val,y_val in zip(x_data,y_data):
+        y_pred_val=forward(x_val)
+        loss_val=loss(x_val,y_val)
+        l_sum+=loss_val
+        print('\t',x_val,y_val,y_pred_val,loss_val)
+    print('MSE=',l_sum/3)
+    w_list.append(w)
+    mse_list.append(l_sum/3)
+
+plt.plot(w_list,mse_list)
+plt.ylabel('loss')
+plt.xlabel('w')
+plt.show()
